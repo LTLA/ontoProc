113d0bff1e00d545d562a25ee5d362512a60a294
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-09-02 23:50:57 -0700

    Allow pruning of search space by ignoring descendents. Point out observed terms with no blessed parents. Acknowledge missing terms in head. Nicer counting of remaining terms.

--- R/chooseBlessedTerms.R
+++ R/chooseBlessedTerms.R
@@ -26,7 +26,7 @@
 #' 
 #' @export
 #' @importFrom igraph make_graph topo_sort neighbors 
-chooseBlessedTerms <- function(onto, ..., skip.empty=TRUE, n=20, saved=NULL) {
+chooseBlessedTerms <- function(onto, ..., skip.empty=TRUE, n=50, saved=NULL) {
     parents <- onto$parents
     self <- rep(names(parents), lengths(parents))
     g <- make_graph(rbind(unlist(parents), self))
@@ -58,22 +58,25 @@ chooseBlessedTerms <- function(onto, ..., skip.empty=TRUE, n=20, saved=NULL) {
     }
    
     # Running through the terms.
-    for (i in seq_along(ordering)) {
-        o <- ordering[i]
+    processed <- sum(!is.na(saved$used))
+    for (o in ordering) {
         if (!is.na(saved$used[o])) {
             next
         }
 
-        children <- setdiff(names(subcomponent(g, o, "out")), o)
-        children <- intersect(children, all.nodes)
-        if (skip.empty && length(children)==0L) {
+        processed <- processed + 1L
+        descendents <- setdiff(names(subcomponent(g, o, "out")), o)
+        ancestors <- setdiff(names(subcomponent(g, o, "in")), o)
+
+        observed <- intersect(descendents, all.nodes)
+        if (skip.empty && length(observed)==0L) {
             saved$used[o] <- FALSE
             next
         }
 
         cat(strrep("*", 80))
         cat("\n")
-        cat(sprintf("Current term is %s (%s, %i/%i)\n", o, onto$name[o], i, length(ordering)))
+        cat(sprintf("Current term is %s (%s) (%i remaining)\n", o, onto$name[o], length(ordering) - processed + 1L))
         cat("\n")
 
         # Gathering all of the children.
@@ -81,38 +84,63 @@ chooseBlessedTerms <- function(onto, ..., skip.empty=TRUE, n=20, saved=NULL) {
         cat("####### Children  #######\n")
         cat("#########################\n\n")
 
-        direct.children <- names(neighbors(g, o, mode="out"))
-        df <- data.frame(name=onto$name[direct.children])
-        rownames(df) <- direct.children
+        children <- names(neighbors(g, o, mode="out"))
+        df <- data.frame(name=onto$name[children])
+        rownames(df) <- children
         print(head(df, n))
+        if (n < nrow(df)) {
+            cat(sprintf("... plus %i more term(s)\n", nrow(df) - n))
+        }
         cat("\n")
         
         cat("#########################\n")
         cat("###### Descendents ######\n")
         cat("#########################\n\n")
 
-        df <- data.frame(name=onto$name[children], mat[children,,drop=FALSE])
-        rownames(df) <- children
+        df <- data.frame(name=onto$name[observed], mat[observed,,drop=FALSE])
+        rownames(df) <- observed 
         print(head(df, n))
+        if (n < nrow(df)) {
+            cat(sprintf("... plus %i more term(s)\n", nrow(df) - n))
+        }
         cat("\n")
+
+        if (any(observed==o) && !any(saved$used[ancestors])) {
+            cat("NOTE: this term is directly observed but has no blessed ancestors.\n\n")
+        }
 
         # Gathering all of the children.
         cat("#########################\n")
         cat("####### Ancestors #######\n")
         cat("#########################\n\n")
 
-        parents <- setdiff(names(subcomponent(g, o, "in")), o)
-        df <- data.frame(name=onto$name[parents], blessed=ifelse(saved$used[parents]=="y", "y", ""))
-        rownames(df) <- parents
+        df <- data.frame(name=onto$name[ancestors], blessed=ifelse(saved$used[ancestors], "y", ""))
+        rownames(df) <- ancestors
         print(head(df, n))
+        if (n < nrow(df)) {
+            cat(sprintf("... plus %i more term(s)\n", nrow(df) - n))
+        }
         cat("\n")
 
         repeat {
-            answer <- readline(sprintf("Bless %s (%s)? [y/n]: ", o, onto$name[o]))
+            answer <- readline(sprintf("Bless %s (%s)? [y/n/a/i]
+  - (y)es
+  - (n)o
+  - bless this term for (a)ll descendents.
+  - (i)gnore this term and all descendents.
+Choice: ", o, onto$name[o]))
             if (answer=="y") {
                 saved$used[o] <- TRUE
             } else if (answer=="n") {
                 saved$used[o] <- FALSE
+            } else if (answer=="a") {
+                saved$used[o] <- TRUE
+                saved$used[descendents] <- TRUE
+                processed <- processed + length(descendents)
+            } else if (answer=="i") {
+                saved$used[o] <- FALSE
+                saved$used[descendents] <- FALSE
+                processed <- processed + length(descendents)
             } else {
                 next
             }
